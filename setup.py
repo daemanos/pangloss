dd33a6afef42b709a19a7ccfe654772514754c56
Octaplex <damanm72@gmail.com>
2018-02-09 21:39:07 -0500

    Put version in module + version bump

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+from pangloss import __version__
+
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -9,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pangloss',
-    version='0.1.0',
+    version=__version__,
     description='A pandoc filter for interlinear glosses',
     long_description=long_description,
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
